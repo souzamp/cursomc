ebcb782f617aeab5357d01f415217c94858a8e12
marcos.souza.anl@gmail.com <marcos.paulo.d.souza@gmail.com>
2019-07-24 17:26:05 -0300

    Testando primeira classe de dominio

--- src/main/java/com/marcos/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resource/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.marcos.cursomc.resource;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcos.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Informática");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 }
