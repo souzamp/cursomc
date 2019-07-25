e2228fe7843662d308044cde902d337f423cb485
marcos.souza.anl@gmail.com <marcos.paulo.d.souza@gmail.com>
2019-07-25 17:43:10 -0300

    Criando repositoy e service para Categoria

---
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.marcos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcos.cursomc.domain.Categoria;
+import com.marcos.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
