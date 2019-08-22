220c58fea0dfd0dd800412f76d80bd990bd1c19a
marcos.souza.anl@gmail.com <marcos.paulo.d.souza@gmail.com>
2019-08-22 19:31:06 -0300

    Método para listar todas as categorias com DTO

--- src/main/java/com/marcos/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/marcos/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.marcos.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcos.cursomc.domain.Categoria;
+import com.marcos.cursomc.dto.CategoriaDTO;
 import com.marcos.cursomc.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -49,5 +52,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO()).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/marcos/cursomc/services/CategoriaService.java
+++ src/main/java/com/marcos/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.marcos.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +43,10 @@ public class CategoriaService {
 			// TODO: handle exception
 			throw new DataIntegrityException("Não é possível excluir uma categoria que têm produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+
 	}
 }
